dac580104e0fc4db57e77471c46d8ebe18d12428
Hadi <hadim69@gmail.com>
2021-10-29 17:44:41 +0330

    ContentBuildWidget checkbox design.

--- lib/widgets/content_build_widget.dart
+++ lib/widgets/content_build_widget.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../src/Util/screen_size_config.dart';
+
 class ContentBuildWidget extends StatelessWidget {
   final String? noteText;
   final bool isCheckBox;
   final bool? isDone;
   final Function(String)? onChangedContent;
   final ValueChanged<bool?> onChangeCheckBox;
+  final Function(String)? onFieldSubmitted;
 
   const ContentBuildWidget({
     Key? key,
@@ -14,25 +17,32 @@ class ContentBuildWidget extends StatelessWidget {
     this.isDone,
     required this.onChangedContent,
     required this.onChangeCheckBox,
+    required this.onFieldSubmitted,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ScreenSizeConfig().init(context);
     if (isCheckBox) {
       return ListTile(
-        leading: const Icon(Icons.list),
+        leading: Icon(
+          Icons.list,
+          size: ScreenSizeConfig.safeBlockHorizontal * 7,
+        ),
         title: TextFormField(
           initialValue: noteText,
-          maxLines: null,
-          keyboardType: TextInputType.multiline,
-          style: const TextStyle(
+          maxLines: 1,
+          textInputAction: TextInputAction.go,
+          style: TextStyle(
             fontSize: 18,
+            decoration: isDone! ? TextDecoration.lineThrough : null,
           ),
           decoration: const InputDecoration(
             border: InputBorder.none,
             hintText: 'Your Content...',
           ),
           onChanged: onChangedContent,
+          onFieldSubmitted: onFieldSubmitted,
         ),
         trailing: Checkbox(
           activeColor: Colors.lightBlueAccent,
