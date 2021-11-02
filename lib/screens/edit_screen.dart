1a9a629921ed1c9c9d879f626fcb6f2a79cf83bd
Hadi <hadim69@gmail.com>
2021-11-02 23:29:20 +0330

    Add BottomAppBar and Stick to top of keyboard.

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_note/src/Util/screen_size_config.dart';
 import 'package:provider/provider.dart';
 
 import '../db/notes_database.dart';
@@ -32,76 +33,104 @@ class _EditScreenState extends State<EditScreen> {
 
   @override
   Widget build(BuildContext context) {
+    ScreenSizeConfig().init(context);
     return isLoading
         ? const Center(child: CircularProgressIndicator())
         : Scaffold(
             appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_outlined),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
               actions: [
-                buildCheckBox(),
-                buildSaveButton(),
-                deleteButton(),
+                buildDeleteButton(),
               ],
             ),
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: TextFormField(
-                      maxLines: 1,
-                      initialValue: title,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Title',
-                      ),
-                      validator: (title) => title != null && title.isEmpty
-                          ? 'The title cannot be empty'
-                          : null,
-                      onChanged: (newTitle) {
-                        setState(() => title = newTitle);
-                      }),
-                ),
-                Consumer<ContentsData>(
-                  builder: (context, contentsData, child) {
-                    contentsList = contentsData.getContentList;
-
-                    return Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: contentsData.getContentsListLength,
-                          itemBuilder: (context, index) {
-                            Content content = contentsList![index];
-                            return ContentBuildWidget(
-                              noteText: content.noteText,
-                              isCheckBox: isCheckBox,
-                              isDone: content.isDone,
-                              onChangeCheckBox: (bool? newValue) {
-                                content.isDone = newValue;
-                                contentsData.updateContent(index, content);
-                              },
-                              onChangedContent: (newNoteText) {
-                                return {
-                                  content.noteText = newNoteText,
-                                  contentsData.updateContent(index, content),
-                                };
-                              },
-                              onFieldSubmitted: (String value) {
-                                contentsData.addContent('', false);
-                              },
-                            );
-                          },
+            body: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextFormField(
+                        maxLines: 1,
+                        initialValue: title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
                         ),
-                      ),
-                    );
-                  },
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Title',
+                        ),
+                        validator: (title) => title != null && title.isEmpty
+                            ? 'The title cannot be empty'
+                            : null,
+                        onChanged: (newTitle) {
+                          setState(() => title = newTitle);
+                        }),
+                  ),
+                  Consumer<ContentsData>(
+                    builder: (context, contentsData, child) {
+                      contentsList = contentsData.getContentList;
+
+                      return Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: contentsData.getContentsListLength,
+                            itemBuilder: (context, index) {
+                              Content content = contentsList![index];
+                              return ContentBuildWidget(
+                                noteText: content.noteText,
+                                isCheckBox: isCheckBox,
+                                isDone: content.isDone,
+                                onChangeCheckBox: (bool? newValue) {
+                                  content.isDone = newValue;
+                                  contentsData.updateContent(index, content);
+                                },
+                                onChangedContent: (newNoteText) {
+                                  return {
+                                    content.noteText = newNoteText,
+                                    contentsData.updateContent(index, content),
+                                  };
+                                },
+                                onFieldSubmitted: (String value) {
+                                  contentsData.addContent('', false);
+                                },
+                              );
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            bottomNavigationBar: BottomAppBar(
+              shape: const CircularNotchedRectangle(),
+              color: Theme.of(context).appBarTheme.backgroundColor,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(
+                  0,
+                  0,
+                  64,
+                  MediaQuery.of(context).viewInsets.bottom + 16.0,
                 ),
-              ],
+                height: MediaQuery.of(context).viewInsets.bottom +
+                    ScreenSizeConfig.safeBlockVertical * 8,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    buildSaveButton(),
+                    buildCheckBox(),
+                  ],
+                ),
+              ),
             ),
           );
   }
@@ -126,41 +155,38 @@ class _EditScreenState extends State<EditScreen> {
   Widget buildSaveButton() {
     final isFormValid = title.isNotEmpty && contentsList![0].noteText != '';
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      child: IconButton(
-        icon: const Icon(Icons.save),
-        tooltip: 'SAVE',
-        color: isFormValid ? Colors.white : Colors.grey,
-        onPressed: () async {
-          if (isFormValid) {
-            await addOrUpdateNote();
-            await addAllContents();
-            Navigator.pop(context);
-          }
-        },
-      ),
+    return IconButton(
+      highlightColor: Colors.transparent,
+      icon: const Icon(Icons.save),
+      tooltip: 'SAVE',
+      color: isFormValid ? Colors.white : Colors.grey,
+      onPressed: () async {
+        if (isFormValid) {
+          await addOrUpdateNote();
+          await addAllContents();
+          Navigator.pop(context);
+        }
+      },
     );
   }
 
   Widget buildCheckBox() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      child: IconButton(
-        icon: const Icon(Icons.check_box),
-        tooltip: 'CheckBox',
-        onPressed: () {
-          // Provider.of<ContentsData>(context, listen: false).addContent('');
-          setState(() {
-            isCheckBox = !isCheckBox;
-          });
-        },
-      ),
+    return IconButton(
+      highlightColor: Colors.transparent,
+      icon: const Icon(Icons.assignment_turned_in_outlined),
+      tooltip: 'CheckBox',
+      onPressed: () {
+        // Provider.of<ContentsData>(context, listen: false).addContent('');
+        setState(() {
+          isCheckBox = !isCheckBox;
+        });
+      },
     );
   }
 
-  Widget deleteButton() {
+  Widget buildDeleteButton() {
     return IconButton(
+      highlightColor: Colors.transparent,
       icon: const Icon(Icons.delete),
       tooltip: 'DELETE',
       onPressed: () async {
