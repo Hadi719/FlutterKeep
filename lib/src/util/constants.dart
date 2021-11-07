41d58b4bf4ac3902ce313978536090ba0bfcff0f
Hadi <hadim69@gmail.com>
2021-11-07 23:53:05 +0330

    Refactoring constants.dart.

--- lib/src/util/constants.dart
+++ lib/src/util/constants.dart
@@ -1,9 +1,14 @@
-import 'package:url_launcher/url_launcher.dart';
-
 //
 // Constants, Urls and ...
 
+/// My email
 String kMail = 'hadim69@gmail.com';
+
+/// My GitHub
 String kUrlGitHub = 'https://github.com/Hadi719';
+
+/// My Linked in
 String kUrlLinkedin = 'https://www.linkedin.com/in/hadimahmoudi69/';
+
+/// My Website
 String kUrlWebsite = 'https://hadimh.ir';
