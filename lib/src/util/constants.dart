cf870c7307eb7d127590972e28367732343fbff4
Hadi <hadim69@gmail.com>
2021-11-07 23:27:18 +0330

    Create Constants file.

---
+++ lib/src/util/constants.dart
@@ -0,0 +1,9 @@
+import 'package:url_launcher/url_launcher.dart';
+
+//
+// Constants, Urls and ...
+
+String kMail = 'hadim69@gmail.com';
+String kUrlGitHub = 'https://github.com/Hadi719';
+String kUrlLinkedin = 'https://www.linkedin.com/in/hadimahmoudi69/';
+String kUrlWebsite = 'https://hadimh.ir';
